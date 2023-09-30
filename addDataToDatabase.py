199de9e71a1a0a6f530759138113feba927e06d3
Payal Narwal <122198919+PayalNarwal@users.noreply.github.com>
2023-09-30 22:56:05 +0530

    Update addDataToDatabase.py

--- addDataToDatabase.py
+++ addDataToDatabase.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import db
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred,{
-    'databaseURL':"https://face-recognition-system-5f72e-default-rtdb.firebaseio.com/"
+    'databaseURL':"https://face-recognition-system"
 })
 
 ref = db.reference('Users')
